1f838f797a78ba8817c6ac1483d074afd14a2c97
Murat <muratkorkmaz8306@gmail.com>
2023-03-06 16:04:06 +0100

    ilk

--- src/test/java/Murat.java
+++ src/test/java/Murat.java
@@ -5,12 +5,6 @@ public class Murat {
 
         System.out.println("Murat in kullanim alani");
 
-        echo "# gitHub_Cukuru" >> README.md
-        git init
-        git add README.md
-        git commit -m "first commit"
-        git branch -M main
-        git remote add origin https://github.com/MuratK06/gitHub_Cukuru.git
-        git push -u origin main
+
     }
 }
